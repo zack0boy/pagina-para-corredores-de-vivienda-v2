94c149bcd7e73f6c6eb83160fa9fbffd6246dacd
Nicklix121 <134036240+Nicklix121@users.noreply.github.com>
2026-06-06 12:50:09 -0400

    actualizacion de app.module en backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,42 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { EmpresasModule } from './empresas/empresas.module';
 
 @Module({
-  imports: [EmpresasModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+
+      host: process.env.DB_HOST,
+
+      port: Number(process.env.DB_PORT),
+
+      username: process.env.DB_USER,
+
+      password: process.env.DB_PASSWORD,
+
+      database: process.env.DB_NAME,
+
+      autoLoadEntities: true,
+
+      synchronize: true,
+    }),
+
+    EmpresasModule,
+  ],
+
   controllers: [AppController],
+
   providers: [AppService],
 })
 export class AppModule {}
