c782e75710a18c34995bccb9815dc90fc55492cd
Nicklix121 <134036240+Nicklix121@users.noreply.github.com>
2026-06-07 22:11:25 -0400

    Fix: Importar CuotasModule en ContratosModule para resolver dependencias

--- backend/src/contratos/contratos.module.ts
+++ backend/src/contratos/contratos.module.ts
@@ -4,10 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Contrato } from './entities/contrato.entity';
 import { ContratosService } from './contratos.service';
 import { ContratosController } from './contratos.controller';
+import { CuotasModule } from '../cuotas/cuotas.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Contrato]),
+    CuotasModule,
   ],
   controllers: [ContratosController],
   providers: [ContratosService],
